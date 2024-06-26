48e52d4da362e7b8ac7336c2bd3136261dc0ed9c
alex <a.schoepe@jpberlin.de>
2024-06-26 15:41:48 +0000

    some changes to webutils: web::ts, web::getContent and web::returnBinary; new: web::mimeType and web::returnText
    FossilOrigin-Name: 6afcbe2d22a21dab5993beca49a9816767984e5c254617a9483d6a7db36ce8a7

--- src/generic/webutils.tcl
+++ src/generic/webutils.tcl
@@ -16,7 +16,7 @@ if {{getrandom} in [web::randombytes names]} {
 }
 
 proc web::ts { {ms {}} } {
-  if {$ms eq {}} {
+  if {![string is wideinteger -strict $ms]} {
      set ms [clock milliseconds]
   }
   return [clock format [expr {$ms / 1000}] -format {%Y-%m-%dT%H:%M:%S}].[format %03d [expr {$ms % 1000}]]
@@ -67,13 +67,40 @@ proc web::uuidV4 {} {
 
 
 proc web::getContent {} {
-  return [encoding convertfrom utf-8 [web::request CONTENT_DATA]]
+  if {[web::request CONTENT_ENCODING] in [encoding names]} {
+    return [encoding convertfrom [web::request CONTENT_ENCODING] [web::request CONTENT_DATA]]
+  }
+  return [web::request CONTENT_DATA]
+}
+
+
+proc web::mimeType { token } {
+  switch -nocase -- $token {
+    bin { return application/octet-stream }
+    bz2 { return application/x-bzip2 }
+    csv { return text/csv }
+    docx { return application/vnd.openxmlformats-officedocument.wordprocessingml.document }
+    gz { return application/gzip }
+    ics { return text/calendar }
+    jpg { return image/jpeg }
+    json { return application/json }
+    png { return image/png }
+    pdf { return application/pdf }
+    rtf { return application/rtf }
+    svg { return image/svg+xml }
+    txt { return text/plain }
+    xlsx { return application/vnd.openxmlformats-officedocument.spreadsheetml.sheet }
+    xml { return application/xml }
+    zip { return application/zip }
+    7z { return application/x-7z-compressed }
+  }
+  return $token
 }
 
 
 proc web::returnJson { json } {
   fconfigure [web::response] -encoding utf-8
-  web::response -set Content-Type {application/json}
+  web::response -set Content-Type [mimeType json]
   web::response -set Content-Length [string length [encoding convertto utf-8 $json]]
   web::response -set Cache-Control {no-cache, no-store, must-revalidate}
   web::response -set Pragma no-cache
@@ -83,18 +110,30 @@ proc web::returnJson { json } {
 }
 
 
-proc web::returnBinary { mimetype data filename } {
-  switch -- $mimetype {
-    pdf {
-      set mimetype application/pdf
-    }
+proc web::returnText { mimetype data { encoding utf-8 } } {
+  if {$encoding ne {} && $encoding in [encoding names]} {
+    fconfigure [web::response] -encoding $encoding
+    web::response -set Content-Length [string length [encoding convertto $encoding $data]]
+  } else {
+    web::response -set Content-Length [string length $data]
   }
+  web::response -set Content-Type [mimeType $mimetype]
+  web::response -set Cache-Control {no-cache, no-store, must-revalidate}
+  web::response -set Pragma no-cache
+  web::response -set Expires 0
+  web::put $data
+  web::response -reset
+}
+
+
+proc web::returnBinary { mimetype data filename } {
   fconfigure [web::response] -translation binary
-  web::response -set Content-Type $mimetype
+  web::response -set Content-Type [mimeType $mimetype]
   web::response -set Content-Length [string length $data]
   web::response -set Content-Disposition "attachment; filename=[file tail $filename]"
   web::response -set Cache-Control {no-cache, no-store, must-revalidate}
   web::response -set Pragma no-cache
   web::response -set Expires 0
   web::put $data
+  web::response -reset
 }
