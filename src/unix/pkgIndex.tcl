bde9fa06001e4a763d83af7c668cc5542f04806a
alex <a.schoepe@jpberlin.de>
2025-02-17 20:41:16 +0000

    Version-3.7.3
    FossilOrigin-Name: 996e75b4ff0412e28a2111cb355fd752e5bbbee8d51033c2f853bfb560ab5222

--- src/unix/pkgIndex.tcl
+++ src/unix/pkgIndex.tcl
@@ -1,2 +1,2 @@
-package ifneeded websh 3.7.2 [list load [file join \
-        $dir libwebsh3.7.2[info sharedlibextension]]]
+package ifneeded websh 3.7.3 [list load [file join \
+        $dir libwebsh3.7.3[info sharedlibextension]]]
